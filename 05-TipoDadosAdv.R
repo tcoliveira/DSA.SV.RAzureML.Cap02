42c8838f057a62858a0ae9e8541686f9cc2e4fb9
Thiago Carlos de Oliveira <thiagocarlos09@gmail.com>
2019-04-17 17:32:04 -0300

    Update 05-TipoDadosAdv.R
    Completo, com array e dtaframe

--- 05-TipoDadosAdv.R
+++ 05-TipoDadosAdv.R
@@ -14,3 +14,23 @@ matriz1
 
 array1<-array(1:5, dim=c(3,3,3))
 array1
+#Dataframe, diversas linhas / diversos tipos de dados
+
+View(iris)
+length(iris)
+
+#Lista de mais de um tipo de dados.
+
+lista1<-list(a = matriz1, b = vetor1)
+lista1
+length(lista1)
+
+#Funcoes, sao considerados objetos
+
+func1<- function(x){
+  var1 <- x * x
+    return(var1)
+}
+func1(5)
+objects()
+rm(array1, func1)
