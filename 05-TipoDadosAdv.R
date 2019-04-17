3303bd05d87c6e37b2ae77a1acbfe090a0847f31
Thiago Carlos de Oliveira <thiagocarlos09@gmail.com>
2019-04-17 17:19:24 -0300

    Update 05-TipoDadosAdv.R
    Tipos de dados Vetor

--- 05-TipoDadosAdv.R
+++ 05-TipoDadosAdv.R
@@ -1,1 +1,16 @@
+#Vetor - possui uma dimensaão e um tipo de dados
+vetor1<-c(1:20)
+vetor1
+length(vetor1)
+mode(vetor1)
 
+#Matriz: possui 2 dimensoes e um tiopo de dados
+
+matriz1<- matrix(1:20, nr = 2)
+matriz1
+
+
+#Array: possui duas ou mais dimensões e um tipo de dados
+
+array1<-array(1:5, dim=c(3,3,3))
+array1
