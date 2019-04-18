5c20023eea9e1b8867be1a767fb3428e74080960
Thiago Carlos de Oliveira <thiagocarlos09@gmail.com>
2019-04-18 12:32:28 -0300

    Estudo vetores

---
+++ 06-Vetores.R
@@ -0,0 +1,42 @@
+getwd()
+vetor_chac= c('Data',"Science",'academy')
+
+#floats
+
+vetor_num = c(1.9, 4.5, 8.9)
+vetor_num
+
+#Vetor complexo
+
+vetor_comp = c(5+3i, 6.7+9i)
+vetor_comp
+
+#vetor logico
+vet_log = c(TRUE,FALSE,FALSE,TRUE)
+vet_log
+
+#vETOR DE INTEIROS
+vetor_int = c(1,2,3)
+
+#Utilizando seq()
+vetor1 = seq(1:5)
+is.vector(vetor1)
+
+#Indexação de vetores
+
+a<-c(1,2,3,4,5)
+a
+a[1]
+a[6]
+
+
+#combinando vetores
+
+v1 <- c(2,4,5)
+v2<- c('a','b')
+
+c(v1,v2)
+
+#Operações com vetores
+
+v1*5
