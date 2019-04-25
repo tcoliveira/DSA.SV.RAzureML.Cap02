1aeaeeb46a2452e0c9b4c4add80b3d80d073309a
Thiago Carlos de Oliveira <thiagocarlos09@gmail.com>
2019-04-25 09:54:18 -0300

    String completo

--- 09-Strings.R
+++ 09-Strings.R
@@ -17,3 +17,19 @@ sprintf('A %s eh nota %d', 'DsA', 10)
 #Extraindo parte da string
 substr(texto, start = 12, stop=17)
 nchar(texto)
+#usandro stringr
+
+strsplit('Histogramas e elementos de dados', NULL)
+
+#Trabalhando com strings
+
+str1<-c('Esta é uma string e sera a primeira parte')
+str1
+str2<-c('testar ourtas str')
+str2
+#adicionando duas strings
+str_c(c(str1, str2), sep ="")
+
+str_count(str2, "s")
+
+str_locate_all(str2, 's')
