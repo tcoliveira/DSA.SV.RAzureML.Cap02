fa2f6acf2397d7a16f7e5b3af3680ddfc3018e20
Thiago Carlos de Oliveira <thiagocarlos09@gmail.com>
2019-04-29 18:35:01 -0300

    Dataframe primeira parte

--- 10-Dataframes.R
+++ 10-Dataframes.R
@@ -1,4 +1,3 @@
-setwd("C:/DSA/RAzureMachineLearning/CursoR/DSA.SV.RAzureML.Cap02")
 df<-data.frame()
 class(df)
 df
@@ -8,27 +7,33 @@ itens<-numeric()
 codigos<-integer()
 df<-data.frame(c(nomes, idades, itens, codigos))
 df
+
 #Criando vetores
 pais = c('Portugal', 'Inglaterra', 'Irlanda', 'Egito', 'Brasil')
 nome = c('Bruno','Tiago', 'Amanda', 'Bianca', ' Marta')
-altura = c(1.66, 1.88, 1.76, 1.67)
-codigo = c(5001, 2183, 4702, 8890)
-pesquisa = data.frame(pais, nome, altura, codigo)
 altura = c(1.66, 1.88, 1.76, 1.67, 1.98)
 codigo = c(5001, 2183, 4702, 8890, 7965)
+
+#Criando um dataframe de diversos fatores
+
 pesquisa = data.frame(pais, nome, altura, codigo)
 pesquisa
+
+
 #add a um dataframe ja existente
 olhos = c('verde', 'castanho', 'verde', 'azul', 'castanho')
 pesq = cbind(pesquisa, olhos)
 pesq
-pesq
+
 #Informações sobre o df
 str(pesq)
 dim(pesq)
 length(pesq)
+
 #Obtendo um vetor de um dataframe
 pesq$pais
+
 #Extraindo um unico valor, slice
 pesq[1,1]
+
 head(pesq)
