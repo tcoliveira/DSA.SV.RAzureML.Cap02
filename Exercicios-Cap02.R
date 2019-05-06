285ac7834b6a49c570c44d230fbfa9edc505f28d
Thiago Carlos de Oliveira <thiagocarlos09@gmail.com>
2019-05-06 14:50:33 -0300

    Exercicios

---
+++ Exercicios-Cap02.R
@@ -0,0 +1,57 @@
+# Lista de Exercícios - Capítulo 2
+
+# Obs: Caso tenha problemas com a acentuação, consulte este link:
+# https://support.rstudio.com/hc/en-us/articles/200532197-Character-Encoding
+
+# Configurando o diretório de trabalho
+# Coloque entre aspas o diretório de trabalho que você está usando no seu computador
+# Não use diretórios com espaço no nome
+setwd("C:/FCD/BigDataRAzure/Cap02")
+getwd()
+
+# Exercício 1 - Crie um vetor com 30 números inteiros
+vet = c(1:30)
+vet
+# Exercício 2 - Crie uma matriz com 4 linhas e 4 colunas preenchida com números inteiros
+mat = matrix(1:16, nr = 4, nc= 4)
+
+mat
+# Exercício 3 - Crie uma lista unindo o vetor e matriz criados anteriormente
+lista1 = list(vet, mat)
+lista1
+class(lista1)
+# Exercício 4 - Usando a função read.table() leia o arquivo do link abaixo para uma dataframe
+# http://data.princeton.edu/wws509/datasets/effort.dat
+df1=data.frame(read.table('http://data.princeton.edu/wws509/datasets/effort.dat'))
+df1
+# Exercício 5 - Transforme o dataframe anterior, em um dataframe nomeado com os seguintes labels:
+# c("config", "esfc", "chang")
+colnames(df1)<-c('config','esfc','chang')
+
+
+# Exercício 6 - Imprima na tela o dataframe iris, verifique quantas dimensões existem no dataframe iris e imprima um resumo do dataset
+iris
+dim(iris)
+summary(iris)
+# Exercício 7 - Crie um plot simples usando as duas primeiras colunas do dataframe iris
+
+
+plot(iris$Sepal.Length,iris$Sepal.Width)
+
+# Exercício 8 - Usando a função subset, crie um novo dataframe com o conjunto de dados do dataframe iris em que Sepal.Length > 7
+# Dica: consulte o help para aprender como usar a função subset()
+?subset
+irisfiltro=subset(iris, iris$Sepal.Length>7)
+
+
+# Exercícios 9 (Desafio) - Crie um dataframe que seja cópia do dataframe iris e usando a função slice(), divida o dataframe em um subset de 15 linhas
+# Dica 1: Você vai ter que instalar e carregar o pacote dplyr
+# Dica 2: Consulte o help para aprender como usar a função slice()
+sub1=slice(iris, 1:15)
+sub1
+# Exercícios 10 - Use a função filter no seu novo dataframe criado no item anterior e retorne apenas valores em que Sepal.Length > 6
+# Dica: Use o RSiteSearch para aprender como usar a função filter
+
+filter(sub1, Sepal.Length > 6 )
+
+
